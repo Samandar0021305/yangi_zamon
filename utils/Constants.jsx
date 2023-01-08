cf7564852911e8f0f430cf3aeae5e6d9c5f928e8
tsamandardev89 <TSamandardev89@gmail.com>
2023-01-08 11:59:38 +0500

    comit

---
+++ utils/Constants.jsx
@@ -0,0 +1,81 @@
+// import PhoneIcon from '@mui/icons-material/Phone';
+// import QueryBuilderIcon from '@mui/icons-material/QueryBuilder';
+
+export const NavList = [
+    {
+        id:1,
+        text:"Biz haqimizda",
+        href:"/haqimizda"
+    },
+    {
+        id:2,
+        text:"Shifokorlar",
+        href:"/Shifokorlar",
+    },
+    {
+        id:3,
+        text:"Aloqa",
+        href:"/Aloqa",
+    },
+    {
+        id:4,
+        text:"FAQ",
+        href:"/Faq",
+    },
+    
+    
+]
+
+
+export const navContact = [
+    {
+        id:1,
+        img:"fa fa-phone",
+        text:"+998 99 231 2433",
+        href:"tel:+998 99 231 2433"
+    },
+    {
+        id:2,
+        img:"fa fa-envelope",
+        text:"support@yangi-zamon.uz",
+        href:"email:support@yangi-zamon.uz"
+    },
+]
+
+
+
+
+
+export const HeaderList = [
+    {
+        id:1,
+        href:"/",
+        text:"Bosh Sahifa"
+    },
+    {
+        id:2,
+        href:"/haqimizda",
+        text:"Biz Haqimizda"
+    },
+    {
+        id:3,
+        href:"/Shifokorlar",
+        text:"Shifokorlar"
+    },
+    {
+        id:4,
+        href:"/vaqt",
+        text:"Ish Vaqti"
+    },
+    {
+        id:5,
+        href:"/Narxlart",
+        text:"Narxlar"
+    },
+    {
+        id:6,
+        href:"/Kassalik",
+        text:"Kassalik"
+    },
+]
+
